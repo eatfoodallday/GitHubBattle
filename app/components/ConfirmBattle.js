d5addc1eae3f97980d549a40768cc7ef294764f5
Jordan Yen <Jordan Yen>
2016-08-22 05:10:43 -0700

    fourth app update

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,10 +1,14 @@
 var React = require('react');
 
+function puke (object) {
+    return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
+
 function ConfirmBattle(props){
     return props.isLoading === true // if isloading is true
     ? <p> LOADING! </p> 
     // print out loading
-    : <p> CONFIRM BATTLE! </p>
+    : <div> CONFIRM BATTLE!: {puke(props)}</div>
     // else confirm battle!
 }
        
